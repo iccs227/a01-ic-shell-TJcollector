c8dd49c1f83f2a0bf9c24a450fe3d7355d8977d1
TJcollector <teeratasspe@gmail.com>
2025-05-27 19:00:10 +0700

    testing the program repeating the input

--- icsh.c
+++ icsh.c
@@ -1,17 +1,25 @@
 /* ICCS227: Project 1: icsh
- * Name:
- * StudentID:
+ * Name: Teeratass Peysantiwong
+ * StudentID: 6480651
  */
 
 #include "stdio.h"
-
+#include <string.h>
 #define MAX_CMD_BUFFER 255
 
 int main() {
     char buffer[MAX_CMD_BUFFER];
     while (1) {
         printf("icsh $ ");
-        fgets(buffer, 255, stdin);
+        if(fgets(buffer, MAX_CMD_BUFFER,stdin)== NULL){
+            printf("\n");
+            break;
+        }
+        buffer[strcspn(buffer, "\n")]=0;
+        if(strlen(buffer)==0){
+            continue;
+        }
+        //fgets(buffer, 255, stdin);
         printf("you said: %s\n", buffer);
     }
 }
