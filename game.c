0a62a1344eb591e9743998eb076066bb489afa5a
TJcollector <teeratasspe@gmail.com>
2025-05-31 23:49:35 +0700

    no time to make a list of words

--- game.c
+++ game.c
@@ -11,7 +11,7 @@
 #include <ctype.h>
 
 void wordle_game() {
-    const char *answer = "fades";  
+    const char *answer = "fades";  //no time to make it as a list/array instead sobbing...
     char guess[100]; // allow room for extra input, we'll trim to 5 chars
     int max_attempts = 6;
 
